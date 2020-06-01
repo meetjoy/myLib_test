a0742a80fab5c503009c833bfeb002e7bceffeaf
Jesse Li <a933476@gmail.com>
2020-06-01 15:15:36 +0800

    useUnifiedTopology: true added to mongoose

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var app = express();
 //Set up mongoose connection
 var mongoose = require('mongoose');
 var mongoDB = 'mongodb://127.0.0.1:27017/';
-mongoose.connect(mongoDB, { useNewUrlParser: true });
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true  });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Hi Jesse, MongoDB connection error:'));
 
