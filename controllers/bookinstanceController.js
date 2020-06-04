9c3b34427be1efdbb4f53ee9f46e2bd73b9ef506
Jesse Li <a933476@gmail.com>
2020-06-04 16:51:01 +0800

    detail page of book, bookinstance, genre, author added

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -13,7 +13,23 @@ exports.bookinstance_list = function(req, res, next) {
       
   };
 
-exports.bookinstance_detail = (req, res) => { res.send ('Not implemented: bookinstance detail' + req.params.id); } ;
+// Display detail page for a specific BookInstance.
+exports.bookinstance_detail = function(req, res, next) {
+
+    BookInstance.findById(req.params.id)
+    .populate('book')
+    .exec(function (err, bookinstance) {
+      if (err) { return next(err); }
+      if (bookinstance==null) { // No results.
+          var err = new Error('Book copy not found');
+          err.status = 404;
+          return next(err);
+        }
+      // Successful, so render.
+      res.render('bookinstance_detail', { title: 'Copy: '+bookinstance.book.title, bookinstance:  bookinstance});
+    })
+
+};
 
 exports.bookinstance_create_get = (req, res) => {res.send('Not implemented: Create bookinstance GET');};
 
